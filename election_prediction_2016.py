a86c46c0a54cd400a2c522c41829ca3bc852f3df
danielbchen <danielbchen@uchicago.edu>
2020-12-28 18:26:24 -0500

    Added func to pivot data

--- election_prediction_2016.py
+++ election_prediction_2016.py
@@ -22,3 +22,32 @@ def personal_income_data_loader():
     df.columns = ['STATE', '2012_PERSONAL_INCOME', '2016_PERSONAL_INCOME']
 
     return df
+
+
+def pivoter(dataframe, year):
+    '''
+    Pivots GDP data and creates new column reporting manufacturing in a given
+    year in a given state as a share of its total GDP for that year.
+    '''
+
+    df = dataframe.copy()
+
+    df = (df
+          .pivot(index='GeoName', columns='Description', values=year)
+          .reset_index())
+
+    df = (df
+          .rename_axis(None, axis=1)
+          .rename_axis('row_num', axis=0)
+          .reset_index())
+
+    cols = ['Manufacturing', 'All industry total']
+    df[cols] = df[cols].astype(float)
+
+    df['MANUFACTURING_SHARE_' + year] = df['Manufacturing'] / \
+        df['All industry total']
+
+    df = df[['GeoName', 'MANUFACTURING_SHARE_' + year]]
+    df.columns = ['STATE', 'MANUFACTURING_SHARE_' + year]
+
+    return df
